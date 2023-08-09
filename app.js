e471ae7c97420020b8b5233fb31787f8e8468027
Lucas D. Gaio <gaio.lucas0@gmail.com>
2023-08-08 21:39:26 -0300

    after installing mysql

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
 app.get('/', (req, res) => {
+
   res.send('Hello World!')
 })
 
